59c95172611c89304b79c37c739e2cf7f6e1b17d
Harman Sandhu <hrrsandhu6@gmail.com>
2020-10-12 14:33:47 -0700

    Test ClientAPI and get AsyncResponse

--- app/src/main/java/com/example/relay/MainActivity.java
+++ app/src/main/java/com/example/relay/MainActivity.java
@@ -3,12 +3,53 @@ package com.example.relay;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements AsyncResponse {
+
+    ClientAPI client = new ClientAPI();
+    TextView textView;
+    String response;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        client.delegate = this;
+        client.execute("Spacex");
+        textView = findViewById(R.id.textView);
+        }
+
+
+    @Override
+    public void processFinish(String output)  {
+        response = output;
+        Log.i("RESULTFINAL", response);
+        try {
+            JSONObject res = new JSONObject(response);
+            Log.i("RESULTFINAL", (String)res.get("totalResults").toString());
+            textView.setText( (String)res.toString());
+        }catch (Exception e){
+            textView.setText(e.getMessage());
+        }
+
     }
 }
+
+//        str
+//
+//        final String sample = {
+//                "author": "fafa",
+//                "title" :" fdfsd",
+//                "desc": "fdsfdsfsd"
+//        }
+//
+//        article_json = APIClient.filter(URL, news_json_string)
+//        article_json = APIClient.filter(URL, news_json_string)
+//        intent.putExtra(ARTICLE, article_json);
