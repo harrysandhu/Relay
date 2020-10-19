d329d731592f8d36aef47270c98395af7ca6cdf2
Harman Sandhu <hrrsandhu6@gmail.com>
2020-10-18 19:52:04 -0700

    NewsArticleActivity download task

--- app/src/main/java/com/example/relay/NewsListActivity.java
+++ app/src/main/java/com/example/relay/NewsListActivity.java
@@ -7,6 +7,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -14,9 +17,11 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class NewsListActivity extends AppCompatActivity implements AsyncResponse {
-    private static final String SEARCH_KEY = "content.search_key";
+    static final String SEARCH_KEY = "content.search_key";
+    static final String ARTICLE = "content.article";
     String searchKey;
     ClientAPI client = new ClientAPI();
     TextView textView;
@@ -28,7 +33,6 @@ public class NewsListActivity extends AppCompatActivity implements AsyncResponse
         client.delegate = this;
         searchKey = getIntent().getStringExtra(MainActivity.SEARCH_KEY);
         client.execute(searchKey);
-        textView = findViewById(R.id.textView);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setHomeButtonEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
@@ -51,20 +55,34 @@ public class NewsListActivity extends AppCompatActivity implements AsyncResponse
             JSONObject res = new JSONObject(output);
             JSONArray articles = res.getJSONArray("articles");
             ArrayList<Article> articleList = new ArrayList<>();
+            final HashMap<String, Article> articleMap = new HashMap<>();
+            ArrayList<String> articleTitles = new ArrayList<>();
             for (int i = 0; i < articles.length(); i++){
-                articleList.add(new Article(articles.getJSONObject(i)));
+                Article a = new Article(articles.getJSONObject(i));
+                articleList.add(a);
+                articleTitles.add(a.getTitle());
+                articleMap.put(a.getTitle(), a);
             }
 
             for (int i = 0; i < articles.length(); i++){
                 Log.i("a", articleList.get(i).toString());
             }
+            Log.i("adapterlist", articleTitles.toString());
 
-            ListView mListView = (ListView) findViewById(R.id.listView);
-            textView.setText(articleList.toString());
-
-            ArticleListAdapter adapter = new ArticleListAdapter(this, R.layout.activity_news_list, articleList);
-            mListView.setAdapter(adapter);
-
+            ListView listView = (ListView) findViewById(R.id.article_list_view);
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list_view_item, articleTitles);
+//            ArticleListAdapter adapter = new ArticleListAdapter(this, R.layout.list_view_item, articleTitles);
+            listView.setAdapter(adapter);
+            final Intent intent = new Intent(this, NewsArticleActivity.class);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    String title = ((TextView)view).getText().toString();
+                    Log.i("MAIN_ACTIVITY", articleMap.get(title).getJsonString());
+                    intent.putExtra(ARTICLE, articleMap.get(title).getJsonString());
+                    startActivity(intent);
+                }
+            });
         }catch (Exception e){
 
         }
